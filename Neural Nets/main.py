868ef48236574b4f487b4aa30678156e985090d5
hashcode55 <blackdragon0909@gmail.com>
2016-08-26 02:45:54 +0530

    regularised model

--- Neural Nets/main.py
+++ Neural Nets/main.py
@@ -7,7 +7,13 @@ import mnsit_load
 
 #get the training data
 #training data is itself a tuple containing inputs and the outputs
-training_data, _, test_data= mnsit_load.load_data_wrapper()
+training_data, validation_data, test_data= mnsit_load.load_data_wrapper()
 
-net = NeuralNetSGD.NeuralNet([784, 30, 10])
-net.stochastic_gradient(training_data, 30, 10, 3.0, test_data = test_data)
+net = NeuralNetSGD.NeuralNet([784, 30, 10], cost = NeuralNetSGD.CrossEntropyCost)
+net.stochastic_gradient(training_data, 30, 10, 0.5, 5.0, 
+	evaluation_data = validation_data, 
+	monitor_evaluation_cost = True,
+	monitor_evaluation_accuracy = True,
+	monitor_training_cost = True, 
+	monitor_training_accuracy = True)
+
